dc30838a16d7063dd6da5badbc509c5599c21c3d
paullaganke <32261820+paullaganke@users.noreply.github.com>
2017-09-26 16:17:04 +0200

    update Notiz

--- iRemember/src/DataStructures/Notiz.java
+++ iRemember/src/DataStructures/Notiz.java
@@ -1,30 +1,39 @@
 package DataStructures;
 
+import java.util.Date;
+
 public class Notiz {
-	
+
 	private long timestamp;
 	private String notiz;
-	
-		public Notiz(String notiztext) {
-			setTimestamp(System.currentTimeMillis());
-			this.setNotiz(notiztext);
-		}
+	private Date date;
 
-		public long getTimestamp() {
-			return timestamp;
-		}
+	public Notiz(String notiztext) {
+		setTimestamp(System.currentTimeMillis());
+		this.setNotiz(notiztext);
+		setDate(new Date(timestamp));
 
-		private void setTimestamp(long timestamp) {
-			this.timestamp = timestamp;
-		}
+	}
 
-		public String getNotiz() {
-			return notiz;
-		}
+	private void setTimestamp(long currentTimeMillis) {
+		// TODO Auto-generated method stub
+		this.timestamp = currentTimeMillis;
+	}
 
-		public void setNotiz(String notiz) {
-			this.notiz = notiz;
-		}
+	public String getNotiz() {
+		return notiz;
+	}
 
-		
+	public void setNotiz(String notiz) {
+		this.notiz = notiz;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 }
