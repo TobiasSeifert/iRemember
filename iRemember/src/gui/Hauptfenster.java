935c935f0bd685e2bc6c1c98979ebbec0921ca24
JohannesMuth <32263713+JohannesMuth@users.noreply.github.com>
2017-09-27 13:27:08 +0200

    gui

--- iRemember/src/gui/Hauptfenster.java
+++ iRemember/src/gui/Hauptfenster.java
@@ -1,16 +1,13 @@
 package gui;
 
 import java.awt.AWTException;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.Image;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
 import java.awt.SystemTray;
+import java.awt.Toolkit;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -20,81 +17,45 @@ import javax.swing.WindowConstants;
 public class Hauptfenster extends JFrame {
 
 	public Hauptfenster() {
-		setLayout(new FlowLayout());
+		setLayout(new BorderLayout());
+				
 		setTitle("iRemember");
-		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setLocation(300, 200);
+		setSize(800, 600);
 		try {
 			setIconImage(ImageIO.read(Hauptfenster.class.getClassLoader().getResourceAsStream("gui/Icon.png")));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		addWindowListener(new TrayListener(this));
-		setSize(800, 600);
-		// pack();
+		
+		
 	}
+public class Test {
+			  Image image = Toolkit.getDefaultToolkit().getImage("gui/Icon.png");
 
-	private class TrayListener extends WindowAdapter {
+			  TrayIcon trayIcon = new TrayIcon(image, "Tester2");
 
-		JFrame frame;
+			  public void main(String[] a) throws Exception {
+			    if (SystemTray.isSupported()) {
+			      SystemTray tray = SystemTray.getSystemTray();
 
-		@Override
-		public void windowClosing(WindowEvent e) {
-			frame.dispose();
-			SystemTray systemTray = SystemTray.getSystemTray();
-			Image pic = null;
-			PopupMenu popup = new PopupMenu();
-			MenuItem defaultItem = new MenuItem("iRemember anzeigen");
-			MenuItem exitItem = new MenuItem("Remember beenden");
-			defaultItem.addActionListener(new TrayActionListener(frame));
-			exitItem.addActionListener(new ExitTrayListener());
+			      trayIcon.setImageAutoSize(true);
+			      trayIcon.addActionListener(new ActionListener() {
+			        public void actionPerformed(ActionEvent e) {
+			          System.out.println("In here");
+			          trayIcon.displayMessage("Tester!", "Some action performed", TrayIcon.MessageType.INFO);
+			        }
+			      });
 
-			try {
-				pic = ImageIO.read(Hauptfenster.class.getClassLoader().getResourceAsStream("gui/Icon.png"));
-			} catch (IOException e1) {
-
-				e1.printStackTrace();
+			      try {
+			        tray.add(trayIcon);
+			      } catch (AWTException e) {
+			        System.err.println("TrayIcon could not be added.");
+			      }
+			    }
+			  }
 			}
-			popup.add(defaultItem);
-			popup.add(exitItem);
-			TrayIcon icon = new TrayIcon(pic, "Öffen...", popup);
-
-			try {
-				systemTray.add(icon);
-			} catch (AWTException awtException) {
-				awtException.printStackTrace();
-			}
-
-		}
-
-		public TrayListener(JFrame frame) {
-			this.frame = frame;
-		}
-	}
-
-	private class TrayActionListener implements ActionListener {
-
-		JFrame frame;
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-
-			frame.setVisible(true);
-		}
-
-		public TrayActionListener(JFrame f) {
-			frame = f;
-		}
-	}
-
-	private class ExitTrayListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			System.exit(0);
-	}
-
-	}
+		
+	
 }
